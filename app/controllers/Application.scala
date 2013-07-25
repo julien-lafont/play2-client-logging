d6f1ca60940c80d96cdcf0591f342f7d08ab2091
Julien Lafont <jla@zenexity.com>
2013-07-26 00:21:08 +0200

    Add fake webpages

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -8,5 +8,24 @@ object Application extends Controller {
   def index = Action {
     Ok(views.html.index())
   }
-  
+
+  def p1 = Action {
+    Ok(views.html.page1())
+  }
+
+  def p2 = Action {
+    Ok(views.html.page2())
+  }
+
+  def p3 = Action {
+    Ok(views.html.page3())
+  }
+
+  def p4 = Action {
+    Ok(views.html.page4())
+  }
+
+  def p5 = Action {
+    Ok(views.html.page5())
+  }
 }
