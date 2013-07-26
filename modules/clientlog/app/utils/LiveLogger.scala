e3c574baa28bb37a4738d414db1b01bb91975b39
Julien Lafont <jla@zenexity.com>
2013-07-26 14:13:38 +0200

    Simplify JsonTransformer in LiveLogger

--- modules/clientlog/app/utils/LiveLogger.scala
+++ modules/clientlog/app/utils/LiveLogger.scala
@@ -6,27 +6,30 @@ import play.api.libs.json._
 
 import org.joda.time.LocalDateTime
 
-case class LiveLogger(clazz : String) {
+case class LiveLogger(clazz: String) {
+
   protected val log = play.api.Logger(clazz)
 
-  def update(path: JsPath, value: JsValue): Reads[JsObject] = path.json.update(Reads.pure(value))
+  private def update(path: JsPath, value: JsValue): Reads[JsObject] = path.json.update(Reads.pure(value))
 
-  def error( error: JsValue)(implicit request : RequestHeader) = {
+  def error(error: JsValue)(implicit request: RequestHeader) = {
     val errorFormated = error.transform(
-      (__).json.update(Reads.pure(Json.obj("ip" -> request.remoteAddress))) andThen
-      (__).json.update(Reads.pure(Json.obj("date" -> new LocalDateTime().toString()))) andThen
-      (__).json.update(Reads.pure(Json.obj("login" -> request.session.get("login").getOrElse("").toString)))
+      (__).json.update(Reads.pure(Json.obj(
+        "ip" -> request.remoteAddress,
+        "date" -> new LocalDateTime().toString(),
+        "login" -> request.session.get("login").getOrElse("").toString
+      )))
     ).get
     controllers.clientlog.Application.channel.push( errorFormated )
     log.error(errorFormated.toString)
   }
 
-  def trace( accessLog :JsValue) = {
-    controllers.clientlog.Application.channel.push( accessLog )
+  def trace(accessLog: JsValue) = {
+    controllers.clientlog.Application.channel.push(accessLog)
     log.trace(accessLog.toString)
   }
 }
 
 object LiveLogger {
-  def apply() : LiveLogger = LiveLogger("application")
+  def apply(): LiveLogger = LiveLogger("application")
 }
