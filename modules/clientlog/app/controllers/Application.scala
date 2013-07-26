5fcd629b3a6b5362d259aa89010213c65ff5e251
Maxime Calmels <mca@zenexity.com>
2013-07-26 10:11:43 +0200

    Broadcast

--- modules/clientlog/app/controllers/Application.scala
+++ modules/clientlog/app/controllers/Application.scala
@@ -4,11 +4,30 @@ import play.api._
 import play.api.mvc._
 
 import play.api.Play.current
+import play.api.libs._
+import play.api.libs.iteratee._
+import play.api.libs.json._
+import scala.language.reflectiveCalls
 
 object Application extends Controller {
 
+  val ( output, channel ) = Concurrent.broadcast[JsValue]
+
   def index = Action {
     Ok(views.html.clientlog.index(current))
+  }
+
+  def live = Action {
+    Ok.feed(
+    output &>
+        EventSource[JsValue]() ><>
+    Enumeratee.map(_.getBytes("UTF-8"))
+      ).as("text/event-stream")
+  }
+
+  def push = Action {
+    channel.push( Json.obj( "error" -> "titi", "name" -> "toto" ))
+    Ok( "Pushed")
   }
 
 }
