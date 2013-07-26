e48efb68b82dc879d48f14629b9cae410b24ffde
Julien Lafont <jla@zenexity.com>
2013-07-26 15:02:02 +0200

    Replace onRouteRequest by AccessFilter

--- app/Global.scala
+++ app/Global.scala
@@ -1,32 +1,7 @@
 
-import play.api._
+import clientlog.filters.AccessLogFilter
 import play.api.mvc._
-import play.api.libs.json._
-import utils._
-import org.joda.time.LocalDateTime
 
-// Note: this is in the default package.
-object Global extends GlobalSettings {
-
-  override def onRouteRequest(request: RequestHeader): Option[Handler] = {
-    val accessJson =
-      Json.obj(
-        "useragent" -> request.headers.get("User-Agent"),
-        "message" -> "",
-        "url" -> request.path,
-        "line" -> "",
-        "level" -> "trace",
-        "ip" -> request.remoteAddress,
-        "date" -> new LocalDateTime().toString(),
-        "login" -> request.session.get("login").getOrElse("").toString
-      )
-
-    if (!(request.path.length >= 7 && request.path.substring(0,7) == "/assets") &&
-      !(request.path.length >= 5 && request.path.substring(0,5) == "/logs")) {
-
-      LiveLogger("access").trace(accessJson)
-    }
-    super.onRouteRequest(request)
-  }
+object Global extends WithFilters(AccessLogFilter) {
 
 }
---
+++ modules/clientlog/app/filters/AccessLogFilter.scala
@@ -0,0 +1,36 @@
+package clientlog.filters
+
+import org.joda.time.LocalDateTime
+import play.api._
+import play.api.mvc._
+
+import play.api.libs.json._
+import play.api.libs.functional.syntax._
+
+import utils.LiveLogger
+
+object AccessLogFilter extends Filter {
+
+  def apply(next: (RequestHeader) => Result)(request: RequestHeader) = {
+
+    val accessJson =
+      Json.obj(
+        "useragent" -> request.headers.get("User-Agent"),
+        "message" -> "",
+        "url" -> request.path,
+        "line" -> "",
+        "level" -> "trace",
+        "ip" -> request.remoteAddress,
+        "date" -> new LocalDateTime().toString(),
+        "login" -> request.session.get("login").getOrElse("").toString
+      )
+
+    if (!(request.path.length >= 7 && request.path.substring(0,7) == "/assets") &&
+      !(request.path.length >= 5 && request.path.substring(0,5) == "/logs")) {
+
+      LiveLogger("access").trace(accessJson)
+    }
+
+    next(request)
+  }
+}
