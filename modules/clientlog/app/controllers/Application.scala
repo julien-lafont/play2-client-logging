37a36e3f241cb15ece6b8195b4609a6e8b0e5c59
Julien Lafont <jla@zenexity.com>
2013-07-26 13:51:21 +0200

    Configure clientlogger from application.conf

--- modules/clientlog/app/controllers/Application.scala
+++ modules/clientlog/app/controllers/Application.scala
@@ -18,6 +18,17 @@ object Application extends Controller {
     Ok(views.html.clientlog.index(current))
   }
 
+  def script = Action { implicit request =>
+    val config = Play.current.configuration.getConfig("clientlog").get
+    val enabled = config.getBoolean("enabled").getOrElse(true)
+    val interval = config.getInt("interval").getOrElse(10)
+    val firstErrorOnly = config.getBoolean("firstErrorOnly").getOrElse(true)
+    val url = controllers.clientlog.routes.Application.pushLog.absoluteURL()
+
+    Ok(views.txt.clientlog.script(url, enabled, interval, firstErrorOnly))
+      .as(JAVASCRIPT)
+  }
+
   def live = Action {
     Ok.feed(
       output &> EventSource[JsValue]() ><> Enumeratee.map(_.getBytes("UTF-8"))
