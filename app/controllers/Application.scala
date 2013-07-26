ea3d2a49c2a4dcbe6986adceed118df2916e010a
Julien Lafont <jla@zenexity.com>
2013-07-26 12:00:40 +0200

    Add Fake login (with ?login=mypony)

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -5,8 +5,12 @@ import play.api.mvc._
 
 object Application extends Controller {
 
-  def index = Action {
-    Ok(views.html.index())
+  def index(login: Option[String] = None) = Action { request =>
+    val result = Ok(views.html.index())
+    login match {
+      case Some(login) => result.withSession("login" -> login)
+      case None => result
+    }
   }
 
   def p1 = Action {
